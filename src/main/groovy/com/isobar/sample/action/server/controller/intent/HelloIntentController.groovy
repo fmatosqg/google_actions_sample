51e769662206636176a15240878e1b7154677a17
Fabio Goncalves <fabio.goncalves@imac-fabio.ap.media.global.loc>
2017-06-06 10:23:15 +1000

    Refactor and add intents

--- src/main/groovy/com/isobar/sample/action/server/controller/intent/HelloIntentController.groovy
+++ src/main/groovy/com/isobar/sample/action/server/controller/intent/HelloIntentController.groovy
@@ -1,5 +1,7 @@
 package com.isobar.sample.action.server.controller.intent
 
+import groovy.transform.CompileStatic
+import org.json.JSONObject
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
@@ -16,35 +18,61 @@ class HelloIntentController extends IntentController {
 
     final Logger LOGGER = LoggerFactory.getLogger(HelloIntentController)
 
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @RequestMapping(value = "test", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     String helloGet() {
 
-        return helloPost(null)
+        def param = [result: [action: 'test']]
+        return helloPost(param)
     }
 
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @RequestMapping(value = "intent", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
     String helloPost(@RequestBody def body) {
 
+        def action = body?.result?.action
+        String param = body?.result?.parameters?.any
 
         LOGGER.info("post hit ${body}")
 
-        def param = null
-
         if (body) {
-            param = body['result']['parameters']['any']
-
-
             def source = body?.originalRequest?.source
             source += "/" + body?.result?.source
-            LOGGER.info("Source $source")
+            LOGGER.info("Action $action |||| Source $source")
         }
+
+        JSONObject result = null
+        switch (action) {
+            case 'animals':
+                result = intentAnimals(param)
+                break
+            case 'hello':
+                result = intentHello(param)
+                break
+            case 'test':
+                result = answer('test')
+                break
+        }
+
+        return result as String
+
+    }
+
+    JSONObject intentAnimals(String s) {
+
+        if (s) {
+            return answer("Hmm, a $s person.")
+        } else {
+            return answer("I didn't get that.")
+        }
+    }
+
+    JSONObject intentHello(String param) {
         if (param) {
             LOGGER.info("Param is $param")
-            return helloAnswer("Hello. Who's $param?")
+            return answer("Hello. Who's $param?")
         } else {
-            return helloAnswer("Hello hello")
+            return answer("Hello hello.")
         }
     }
 }
--- src/main/groovy/com/isobar/sample/action/server/controller/intent/IntentController.groovy
+++ src/main/groovy/com/isobar/sample/action/server/controller/intent/IntentController.groovy
@@ -1,14 +1,16 @@
 package com.isobar.sample.action.server.controller.intent
 
+import org.json.JSONObject
+
 /**
  * Created by fabio.goncalves on 5/06/2017.
  */
 class IntentController {
 
-    protected String helloAnswer(String response) {
-        return "{\"speech\": \"$response\"}"
+    protected JSONObject answer(String response) {
+//        return "{\"speech\": \"$response\"}"
 
-//        def responseObject = [speech: response]
-//        return responseObject
+        JSONObject responseObject = [speech: response]
+        return responseObject
     }
 }
